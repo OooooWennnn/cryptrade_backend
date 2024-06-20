60decdc40374d13eb39cc3dc4d6e96fc52df2fd0
Sumin Han <shan94@myseneca.ca>
2024-06-20 05:18:27 -0400

    Vercel

--- routes/premium_routes.js
+++ routes/premium_routes.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const {
   getAllCoins_Ndax,
   getAllCoins_Bithum,
-} = require('../controllers/premium_controller');
+} = require("../controllers/premium_controller");
 
-router.get('/Ndax', getAllCoins_Ndax);
-router.get('/Bithum', getAllCoins_Bithum);
+router.get("/Ndax", getAllCoins_Ndax);
+router.get("/Bithum", getAllCoins_Bithum);
 
 module.exports = router;
